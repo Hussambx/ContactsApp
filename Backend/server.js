aac69e911b98652c445d2509d5a5b24e5c7ba0e7
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-09-04 14:31:16 -0400

    Setup port number and tested server

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,18 @@
-const express =require('express')
+require('dotenv').config()
+const express =require('express');
+
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log(req.path)
+    next()
+})
+
+app.get('/',(req,res)=>{
+    res.json({mssg:'Test Has Passed '})
+})
+
+app.listen(process.env.PORT, ()=> {
+console.log("test")
+})
