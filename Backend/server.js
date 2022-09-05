7902b2ae900cd1ea8a8dbbbc9c8df1e50ec99d26
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-09-04 20:35:08 -0400

    Connected to Mongo Db, and routed to other file

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,30 @@
 require('dotenv').config()
 const express =require('express');
-
+const { default: mongoose } = require('mongoose');
+const moongoose = require('./routes/contacts')
+const contactsRoutes = require('./routes/contacts')
 
 const app = express();
+
+app.use(express.json())
 
 app.use((req,res,next)=>{
     console.log(req.path)
     next()
 })
 
-app.get('/',(req,res)=>{
-    res.json({mssg:'Test Has Passed '})
+//Routes
+app.use('/api/contacts',contactsRoutes)
+
+
+mongoose.connect(process.env.MONGO_URl)
+.then(()=>{
+    app.listen(process.env.PORT, ()=> {
+        console.log("test")
+        })
+})
+.catch((error)=>{
+
 })
 
-app.listen(process.env.PORT, ()=> {
-console.log("test")
-})
+
