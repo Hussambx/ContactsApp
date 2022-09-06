b18aca40d8e98cf655721c1e89c29caa52f8fe47
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-09-05 21:50:06 -0400

    Added React Router to app.jsx + Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+       
+        <Route
+        path='/'
+        element={<h1>Test</h1>}>
+
+
+        </Route>
+      </Routes>
+      
+      
+      </BrowserRouter>
    <h1>Hi</h1>
     
     </div>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import {Link} from 'react-router-dom'
+export default function Navbar(){
+    return(
+        <div>
+            <Link to='/'>
+            <h2>Test Nav Bar</h2>
+            </Link>
+       
+
+        </div>
+        
+    )
+}
