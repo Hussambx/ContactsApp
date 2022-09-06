761de1dbe89b509ba00f8bb9e31536712a53cf2e
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-09-06 11:25:07 -0400

    Improved Navbar.jsx + CSS Styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import {Link} from 'react-router-dom'
 export default function Navbar(){
     return(
-        <div>
-            <Link to='/'>
-            <h2>Test Nav Bar</h2>
-            </Link>
-       
-
+      <div id="navdiv" >
+      <nav>
+<h1>Contact App</h1>
+        </nav>
+        <form>
+<input type="string" placeholder="Search For Contact"></input>
+</form>
         </div>
+            
+            
         
     )
 }
