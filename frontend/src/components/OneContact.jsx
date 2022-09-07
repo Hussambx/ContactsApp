4d61af39593256d9cbf6d9d68a495e0d196393f4
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-09-07 11:01:37 -0400

    OneContact.jsx now allows user to delete contacts

--- frontend/src/components/OneContact.jsx
+++ frontend/src/components/OneContact.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
 
 export default function OneContact(props){
 
+    const[deleteit,SetDelete]=useState(true); //This state tracks of the confirm delete menu should be rendered or not 
+  
     //This function will trigger the editcontactf () in app.jx. it will then update the edit contact state. Which will basically pass the aleady excisting fetched data to the
     //newcontact.jsx field instead of having to fetch the data again 
     function existingvalues(){
         props.gocreation([props.firstname,props.lastname,props.phonenumber,props.id]);
     }
 
+    //This function is triggered once the user hits on yes to confirm the delete request 
+    async function removecontact(){
+        const response = await fetch('http://localhost:4000/api/contacts/'+props.id, {
+            method: 'DELETE',
+          })
+          const json = await response.json()
 
+          if(response.ok){
+            SetDelete(true);
+            props.goback();
+          }
+        }
 
-
+        //This function is triggered to either get the confirm delete menu to appear or to get rid of it 
+        function changedmind(){
+            SetDelete(!deleteit)
+        }
 
     return(
         <div className="onecontact">
@@ -69,7 +86,12 @@ export default function OneContact(props){
             <h4>Add to Favorites</h4>
         </div>
         
-        <button>Delete Contact</button>
+        {deleteit &&<button onClick={changedmind}>Delete Contact</button>}
+        {!deleteit && <h2>Are you sure you want to delete?</h2> }
+        {!deleteit && <button onClick={removecontact}>Yes</button> }
+        {!deleteit && <button onClick={changedmind}>No</button> }
+
+
         </div>
 
     )
