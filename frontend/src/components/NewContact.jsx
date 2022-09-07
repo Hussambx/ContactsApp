cca11ea4e2f9f442f1a1363effa5c6f55017fcd4
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-09-07 10:45:30 -0400

    Newcontact.jsx now uses either PATCH OR POST

--- frontend/src/components/NewContact.jsx
+++ frontend/src/components/NewContact.jsx
@@ -7,10 +7,39 @@ export default function NewContact(props){
         const [firstname,SetFirstname] = useState(props.firstname);
         const [lastname, SetLastname] = useState(props.lastname);
         const [phonenumber,SetNumber]=useState(props.phonenumber);
-        const[idofdata,SetDataId]=useState(props.id)
+        const[idofdata,SetDataId]=useState(props.idofdata)
         const[email,SetEmail] = useState(); //Will add email functionality at a later time 
 
-        //This function submits the form data to the api, to store the new entry into the database once complete will then re-direct to main contacts list page with updated contact
+        //The Main Function called on decides if it will then use the POST method (Create New Contact) or PATCH method (Update excisting contact) based on if idofdata is null 
+        function mainsubmit(){
+            idofdata==null?submit():updatedata();
+        }
+        
+        //This function will use the PATCH method to update the already existing data with the newly created changes 
+        async function updatedata(){
+            const contactinfo = {firstname, lastname, phonenumber}
+            const response = await fetch('http://localhost:4000/api/contacts/'+idofdata, {
+              method: 'PATCH',
+              body: JSON.stringify(contactinfo),
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            })
+            const json = await response.json()
+            if(!response.ok){
+                console.log("Error happened bro")
+            }
+            if(response.ok){
+                //Resets state values as data was sent to db
+                props.goback("");
+                console.log("MINT");
+                SetFirstname('');
+                SetLastname('');
+                SetNumber('');
+            }
+        }
+
+        //This function submits the form data to the api using the POST Method, to store the new entry into the database once complete will then re-direct to main contacts list page with updated contact
         async function submit(){
             const contactinfo = {firstname, lastname, phonenumber}
             const response = await fetch('http://localhost:4000/api/contacts/', {
@@ -40,7 +69,7 @@ export default function NewContact(props){
         
         <div className="menuoptions">
                 <button onClick={props.cancelcreation}>Cancel</button> 
-                <button onClick={submit}>Done</button> 
+                <button onClick={mainsubmit}>Done</button> 
             </div>
          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/2048px-Circle-icons-profile.svg.png"/>
             
