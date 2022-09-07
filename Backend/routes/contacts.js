a64a33f88fca7afa1bb0fe9c90bbfcb683b77e9a
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-09-07 17:36:59 -0400

    Added new route GET method to search by name

--- Backend/routes/contacts.js
+++ Backend/routes/contacts.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const {createContact,getContacts,getContact,deleteContact,updateContact} =require('../controllers/contactController')
+const {createContact,getContacts,getContact,deleteContact,updateContact,searchContacts} =require('../controllers/contactController')
 
 //Get all contacts
 router.get('/',getContacts)
 
 //Find one contact
 router.get('/:id',getContact)
+
+
+//Find contact by name
+router.get('/search/:id',searchContacts)
+
 
 //Create new contact
 router.post('/',createContact)
